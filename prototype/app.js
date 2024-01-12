eae240db7fafbee4d07a2440813c9c2dd30a9465
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2024-01-12 16:01:49 -0600

    All toggleable layers transitioned, no date slider yet.

--- prototype/app.js
+++ prototype/app.js
@@ -70,99 +70,87 @@ VIEWER.updateGeometry = function(event) {
 VIEWER.init = async function() {
     let latlong = [12, 12] //default starting coords
     let locationData = await fetch("./data/AllLocations.json").then(resp => resp.json()).catch(err => {return {}})
-    let pData = await fetch("./data/KastorPeopleNY.json").then(resp => resp.json()).catch(err => {return {}})
-    let taxBoundaries = await fetch("./data/1798_Tax_Divisions_Merged.json").then(resp => resp.json()).catch(err => {return {}})
+    let specificPeople = await fetch("./data/KastorPeopleNY.json").then(resp => resp.json()).catch(err => {return {}})
+    let tax_1798 = await fetch("./data/1798_Tax_Divisions_Merged.json").then(resp => resp.json()).catch(err => {return {}})
     let districtBoundaries = await fetch("./data/1814_Districts_Merged.json").then(resp => resp.json()).catch(err => {return {}})
-    let countyBoundaries = await fetch("./data/CountyBoundaries.json").then(resp => resp.json()).catch(err => {return {}})
+    let tax_1814 = await fetch("./data/CountyBoundaries.json").then(resp => resp.json()).catch(err => {return {}})
     let stateBoundaries = await fetch("./data/StateBoundaries.json").then(resp => resp.json()).catch(err => {return {}})
+    let countyBoundaries = await fetch("./data/CountyBoundaries.json").then(resp => resp.json()).catch(err => {return {}})
     let judicialDistricts = await fetch("./data/judicial_districts.json").then(resp => resp.json()).catch(err => {return {}})
     let judicialCircuits = await fetch("./data/judicial_circuits.json").then(resp => resp.json()).catch(err => {return {}})
     let geoJsonData = []
     let peopleFields = []
     //loadInput.value = "Apply Options"
-    
     let peopleData = []
-    // I want the counties to know their district
-    countyBoundaries.features = countyBoundaries.features.filter(cnty => cnty?.properties?.STATE_TERR === "Pennsylvania")
-    .map(cntyObj => {
-        cntyObj.properties.districts = judicialDistricts.filter(dist => dist.ID === cntyObj.properties.ID)
-        cntyObj.properties.circuits = judicialCircuits.filter(circuit => circuit?.State === "PA")
-        return cntyObj
+    let geoJsonByLayers = {}
+
+    // Format FeatureCollections' Features Array so each feature known its _name for filtering.
+    tax_1798.features = tax_1798.features.map(f => {
+        if(!f.hasOwnProperty("properties")) f.properties = {}
+        f.properties._name = tax_1798._name
+        return f
+    })
+    tax_1814.features = tax_1814.features.map(f => {
+        if(!f.hasOwnProperty("properties")) f.properties = {}
+        f.properties._name = tax_1814._name
+        return f
+    })
+    countyBoundaries.features = countyBoundaries.features.map(f => {
+        if(!f.hasOwnProperty("properties")) f.properties = {}
+        f.properties._name = countyBoundaries._name
+        return f
+    })
+    stateBoundaries.features = stateBoundaries.features.map(f => {
+        if(!f.hasOwnProperty("properties")) f.properties = {}
+        f.properties._name = stateBoundaries._name
+        return f
+    })
+    locationData.features = locationData.features.map(f => {
+        if(!f.hasOwnProperty("properties")) f.properties = {}
+        f.properties._name = locationData._name
+        return f
     })
 
-    // I want the states (' counties'?) to know their circuit
-    stateBoundaries.features = stateBoundaries.features.filter(state => state?.properties.ABBR_NAME === "PA")
-    .map(stateObj => {
-        stateObj.properties.circuits = judicialCircuits.filter(circuit => circuit?.State === stateObj.properties.ABBR_NAME)
-        return stateObj
+
+    // TODO new funtionality prototyping. I want the counties to know their district
+    // countyBoundaries.features = countyBoundaries.features.filter(cnty => cnty?.properties?.STATE_TERR === "Pennsylvania")
+    // .map(cntyObj => {
+    //     cntyObj.properties.districts = judicialDistricts._data.filter(dist => dist.ID === cntyObj.properties.ID)
+    //     cntyObj.properties.circuits = judicialCircuits._data.filter(circuit => circuit?.State === "PA")
+    //     return cntyObj
+    // })
+
+    //  TODO new funtionality prototyping.  I want the states to know their circuit
+    // stateBoundaries.features = stateBoundaries.features.filter(state => state?.properties.ABBR_NAME === "PA")
+    // .map(stateObj => {
+    //     stateObj.properties.circuits = judicialCircuits._data.filter(circuit => circuit?.State === stateObj.properties.ABBR_NAME)
+    //     return stateObj
+    // })
+
+    specificPeople["_fields"].forEach((element) => {
+        peopleFields.push(element["Fied"])
+    })
+    peopleFields.push("Geocoding ID")
+
+    specificPeople._data.map((item) => {
+        const filteredItem = {}
+        peopleFields.forEach((field) => {
+          if (item.hasOwnProperty(field)) {
+            filteredItem[field] = item[field]
+          }
+        })
+        peopleData.push(filteredItem)
     })
 
-    geoJsonData.push(countyBoundaries)
-    geoJsonData.push(stateBoundaries)
-
-    // locationData = locationData[0]
-    // pData = pData[0]
-    // countyBoundaries = countyBoundaries[0]
-    // stateBoundaries = stateBoundaries[0]
-    // taxBoundaries = taxBoundaries[0]
-    // districtBoundaries = districtBoundaries[0]
-    // pData["Fields"].forEach((element) => {
-    //     peopleFields.push(element["Fied"])
-    // })
-    // peopleFields.push("Geocoding ID")
-
-    // pData["Data"].map((item) => {
-    //     const filteredItem = {}
-    //     peopleFields.forEach((field) => {
-    //       if (item.hasOwnProperty(field)) {
-    //         filteredItem[field] = item[field]
-    //       }
-    //     })
-    //     peopleData.push(filteredItem)
-    // })
-
-    // peopleData.sort(function (a, b) {
-    //     return a.GovernmentEmployeeNumber - b.GovernmentEmployeeNumber
-    // })
-
-    // locationData.features.forEach(function (loc) {
-    //     let tempX = loc.geometry.coordinates[0]
-    //     let tempY = loc.geometry.coordinates[1]
-    //     loc.geometry.coordinates[0] = tempY
-    //     loc.geometry.coordinates[1] = tempX
-    //     loc.properties["Earliest Record"] = parseInt(loc.properties["Earliest Record"])
-    //     loc.properties["Latest Record"] = parseInt(loc.properties["Latest Record"])
-
-    //     let personnelArr = {}
-    //     let temp = peopleData.filter(function (p) {
-    //       return p["Geocoding ID"] == loc.properties["Geocode Number"]
-    //     })
-    //     if (temp.length > 0) {
-    //       temp.forEach((item, index) => {
-    //         const existingId = Object.keys(personnelArr).find((id) =>
-    //           personnelArr[id].includes(item)
-    //         )
-    //         if (existingId) {
-    //           personnelArr[existingId].push(item)
-    //         } else {
-    //           const newId = item["GovernmentEmployeeNumber"]
-    //           personnelArr[newId] = [item]
-    //         }
-    //       })
-    //     }
-    //     loc.properties.personnel = personnelArr
-    // })
-
-    // geoJsonData.push(locationData)
-    // geoJsonData.push(countyBoundaries)
-    // geoJsonData.push(stateBoundaries)
-    // geoJsonData.push(taxBoundaries)
-    // geoJsonData.push(districtBoundaries)
-
-    const formattedGeoJsonData = geoJsonData.flat(1) //AnnotationPages and FeatureCollections cause arrays in arrays.  
-    //Abstracted.  Maybe one day you want to VIEWER.initializeOtherWebMap(latlong, allGeos)
-    console.log(formattedGeoJsonData)
-    VIEWER.initializeLeaflet(latlong, formattedGeoJsonData)
+    peopleData.sort(function (a, b) {
+        return a.GovernmentEmployeeNumber - b.GovernmentEmployeeNumber
+    })
+    geoJsonByLayers.locations = locationData
+    geoJsonByLayers.counties = countyBoundaries
+    geoJsonByLayers.states = stateBoundaries
+    geoJsonByLayers.tax_1798 = tax_1798
+    geoJsonByLayers.tax_1814 = tax_1814
+    VIEWER.initializeLeaflet(latlong, geoJsonByLayers) 
 }
 
 /**
@@ -201,12 +189,6 @@ VIEWER.initializeLeaflet = async function(coords, geoMarkers) {
         maxZoom: 19
     })
 
-    VIEWER.mymap = L.map('leafletInstanceContainer', {
-        center: coords,
-        zoom: 2,
-        layers: [osm, esri_street, topomap, mapbox_satellite_layer]
-    })
-
     let baseMaps = {
         "OpenStreetMap": osm,
         "CartoDB": carto,
@@ -216,103 +198,166 @@ VIEWER.initializeLeaflet = async function(coords, geoMarkers) {
         "USGS Topo + Street": USGS_top_streets,
         "Mapbox Satellite": mapbox_satellite_layer
     }
-    let layerControl = L.control.layers(baseMaps, {}).addTo(VIEWER.mymap)
 
-    // let overlayMaps = {
-    //     "Cities": osm,
-    //     "Streets": esri_street,
-    //     "Satellite" : mapbox_satellite_layer,
-    //     "Topography" : topomap
-    // }
-    //var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(VIEWER.mymap)
+    const stateFeatures = L.geoJSON(geoMarkers.states, {
+        style: function(feature) {
+            let name = feature.properties._name ?? ""
+            return {
+                color: "#005A9C",
+                fillColor: "#005A9C",
+                fillOpacity: 0.00,
+                className: name
+            }
+        },
+        onEachFeature: VIEWER.formatPopup
+    })
 
-    let appColor = "#008080"
-    L.geoJSON(geoMarkers, {
-            pointToLayer: function(feature, latlng) {
-                let __fromResource = feature.properties.__fromResource ?? ""
-                switch (__fromResource) {
-                    case "Type 1":
-                        appColor = "blue"
-                        break
-                    case "Type 2":
-                        appColor = "purple"
-                        break
-                    case "Type 3":
-                        appColor = "yellow"
-                        break
-                    case "Type 4":
-                        appColor = "#008080"
-                        break
-                    case "Type 5":
-                        appColor = "#005A9C"
-                    break
-                    default:
-                        appColor = "red"
-                }
-                return L.circleMarker(latlng, {
-                    radius: 6,
-                    fillColor: appColor,
-                    color: appColor,
-                    weight: 1,
-                    opacity: 1,
-                    fillOpacity: 1
-                })
-            },
-            style: function(feature) {
-                let __fromResource = feature.properties.__fromResource ?? ""
-                switch (__fromResource) {
-                    case "Type 1":
-                        appColor = "blue"
-                        break
-                    case "Type 2":
-                        appColor = "purple"
-                        break
-                    case "Type 3":
-                        appColor = "yellow"
-                        break
-                    case "Type 4":
-                        appColor = "#008080"
-                        break
-                    case "Type 5":
-                        appColor = "#005A9C"
-                    break
-                    default:
-                        appColor = "red"
-                }
-                const ft = feature.geometry.type ?? feature.geometry["@type"] ?? "Yikes"
-                if (ft !== "Point") {
-                    return {
-                        color: appColor,
-                        fillColor: appColor,
-                        fillOpacity: 0.09
-                    }
-                }
-            },
-            onEachFeature: VIEWER.formatPopup
+    const countyFeatures = L.geoJSON(geoMarkers.counties, {
+        style: function(feature) {
+            let name = feature.properties._name ?? ""
+            return {
+                color: "#008080",
+                fillColor: "#008080",
+                fillOpacity: 0.00,
+                className: name
+            }
+        },
+        onEachFeature: VIEWER.formatPopup
+    })
+
+    const taxFeatures1798 = L.geoJSON(geoMarkers.tax_1798, {
+        style: function(feature) {
+            let name = feature.properties._name ?? ""
+            return {
+                color: "blue",
+                fillColor: "blue",
+                fillOpacity: 0.00,
+                className: name
+            }
+        },
+        onEachFeature: VIEWER.formatPopup
+    })
+
+    const taxFeatures1814 = L.geoJSON(geoMarkers.tax_1814, {
+        style: function(feature) {
+            let name = feature.properties._name ?? ""
+            return {
+                color: "purple",
+                fillColor: "purple",
+                fillOpacity: 0.00,
+                className: name
+            }
+        },
+        onEachFeature: VIEWER.formatPopup
+    })
+
+    const locationFeatures = geoMarkers.locations.features.map(f =>{
+        const name = f.properties._name ?? ""
+        return L.circleMarker(f.geometry.coordinates, {
+            radius: 6,
+            fillColor: "yellow",
+            color: "yellow",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 1,
+            className: name
         })
-        .addTo(VIEWER.mymap)
+    })
+    
+    const locationsLayer = L.layerGroup(locationFeatures)
+
+    let main_layers = {
+        "State Boundaries": stateFeatures,
+        "County Boundaries": countyFeatures,
+        "1814 Tax Districts": taxFeatures1814,
+        "1798 Tax Disctricts": taxFeatures1798,
+        "Specific Locations": locationsLayer
+    }
+    VIEWER.mymap = L.map('leafletInstanceContainer', {
+        center: coords,
+        zoom: 2,
+        layers: [
+            osm, 
+            esri_street, 
+            topomap, 
+            mapbox_satellite_layer, 
+            stateFeatures, 
+            countyFeatures, 
+            locationsLayer, 
+            taxFeatures1798, 
+            taxFeatures1814
+        ]
+    })
+    let layerControl = L.control.layers(baseMaps, main_layers).addTo(VIEWER.mymap)
     leafletInstanceContainer.style.backgroundImage = "none"
     loadingMessage.classList.add("is-hidden")
+
+
+    // TODO set the initial view by applying an initial filter.
+    /*
+    var originalCountyData = {
+      ...vis.data[1], // Spread the existing object properties
+      features: vis.data[1].features.filter(function (d) {
+        var sDate = new Date(d.properties["START_DATE"]);
+        var eDate = new Date(d.properties["END_DATE"]);
+        var currDate = new Date("1811-12-31");
+        var bool = sDate < currDate && eDate >= currDate;
+        return bool;
+      }), // Filter the array based on a condition
+    };
+    var originalStateData = {
+      ...vis.data[2], // Spread the existing object properties
+      features: vis.data[2].features.filter(function (d) {
+        var sDate = new Date(d.properties["START_DATE"]);
+        var eDate = new Date(d.properties["END_DATE"]);
+        var currDate = new Date("1811-12-31");
+        var bool = sDate < currDate && eDate >= currDate;
+        return bool;
+      }), // Filter the array based on a condition
+    };
+    var originalLocData = {
+      ...vis.data[0],
+      features: vis.data[0].features.filter(function (d) {
+        var bool = false;
+        if (
+          !isNaN(d.properties["Earliest Record"]) ||
+          !isNaN(d.properties["Latest Record"])
+        ) {
+          bool =
+            d.properties["Earliest Record"] <= 1811 &&
+            d.properties["Latest Record"] >= 1811;
+        }
+        return bool;
+      }),
+    };
+    */
+    // TODO add some kind of fill toggling to allow for "border only" views (per layer?)
+
+    // TODO implement some clustering mechanism for AllLocations
+
+
 }
 
 /**
  * Define what information from each Feature belongs in the popup
  * that appears.  We want to show labels, summaries and thumbnails.
+ * 
+ * TODO do we want this classic popup mechanic or a more static one like on the OG?
  */
 VIEWER.formatPopup = function(feature, layer) {
-    let popupContent = ""
+    let popupContent = "<div id = 'locPopup'><h5>"
     let i = 0
     let langs = []
     let stringToLangMap = {"none":[]}
     if (feature.properties){
-        if (feature.properties.thumbnail) {
-            let thumbnail = feature.properties.thumbnail[0].id ?? feature.properties.thumbnail[0]["@id"] ?? ""
-            popupContent += `<img src="${thumbnail}"\></br>`
+        if (feature.properties["Geocoding Location"]) {
+            popupContent += `${feature.properties["Geocoding Location"]}, <br>"`
         }
-        if (feature.properties.option1) {
-            let manifestURI = feature.properties.option1 ?? ""
-            popupContent += `<a href="https://projectmirador.org/embed/?data-param=${manifestURI}" target="_blank"><img src="https://www.qdl.qa/sites/all/themes/QDLTheme/css/img/logo_mirador.png"/></a>`
-            popupContent += `<a href="https://uv-v3.netlify.app/#?c=&m=&s=&cv=&manifest=${manifestURI}" target="_blank"><img src="https://www.qdl.qa/sites/all/themes/QDLTheme/css/img/logo_uv.png"/></a>`
+        if (feature.properties["State"]) {
+            popupContent += `${feature.properties["State"]} ", "`
+        }
+        if (feature.properties["Country"]) {
+            popupContent += `${feature.properties["Country"]} ", "`
         }
         if (feature.properties.option2) {
             let annoURI = feature.properties.option2 ?? ""
@@ -322,6 +367,9 @@ VIEWER.formatPopup = function(feature, layer) {
                 </div>
             `
         }
+        popupContent += `</h5></div>`
+        layer.options.start = "1888"
+        layer.options.end = "1999"
         layer.bindPopup(popupContent)
     }
 }
